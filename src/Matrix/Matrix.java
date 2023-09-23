0754c5713a65a9a5eeab53725d9bd1f7d2443288
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2023-09-23 13:13:17 +0700

    added Matrix.java

---
+++ src/Matrix/Matrix.java
@@ -0,0 +1,17 @@
+package Matrix;
+
+public class Matrix{
+	static void myMethod(){
+        System.out.println("I just got executed!");
+    }
+    public static void main(String[] args){
+        int[][] m = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+        for(int i = 0; i < m.length; i++){
+            for(int j = 0; j < m.length; j++){
+                System.out.print(m[i][j]);
+            }
+            System.out.println();
+        }
+        myMethod();
+    }
+}
