b9673184780662702fad57fa381abc77578daa90
VansonK <vansonkurnialim@gmail.com>
2023-09-23 19:41:11 +0700

    readMatrix dan readSPL

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -4,34 +4,58 @@ import java.util.Scanner;
 
 public class Matrix{
     static int[][] matrix;
-    static void readMatrix()
+    static int[][] readMatrix()
         {
-            System.out.print("Masukkan baris: ");
             Scanner sc = new Scanner(System.in);
-            int m = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            int n = sc.nextInt();
 
-            matrix = new int[m][n];
-            for (int i = 0; i < m; i++) 
+            System.out.print("Masukkan baris: ");
+            int baris = sc.nextInt();
+
+            System.out.print("Masukkan kolom: ");
+            int kolom = sc.nextInt();
+
+            matrix = new int[baris][kolom];
+            for (int i = 0; i < baris; i++) 
             {
-                for (int j = 0; j < n; j++) 
+                for (int j = 0; j < kolom; j++) 
                 {
+                    System.out.print("Kolom " + (i+1) + " ,Baris " + (j+1) + ": ") ;
                     matrix[i][j] = sc.nextInt();
                 }    
             }
+            return matrix ;
         }
 
-        static void readSPL()
+        static int[][] readSPL()
         {
-            System.out.println("Bentuk Sistem Persamaan Linier yang dapat digunakan:");
-            System.out.println("1. Ax = b");
-            System.out.println("2. Matriks augmented");
-            System.out.println("3. ax1 + bx2 + cx3 = d");
+            System.out.println("Sistem Persamaan berapa variabel : ") ;
+            Scanner sc = new Scanner(System.in) ;
+            int jumlahVar = sc.nextInt() ;
+
+            System.out.println("Berapa persamaan : ") ;
+            int jumlahPers = sc.nextInt() ;
+
+            int[][] matrix = new int[jumlahPers][jumlahVar+1] ;
+
+            for (int i = 0 ; i < jumlahPers ; i++) { // idx 0 itu persamaan pertama terus ngurut terus
+                System.out.print("Persamaan ke-" + i + "\n") ;
+                for (int j = 0 ; j <= jumlahVar ; j++) { // idx 0 itu x1 sampe idx jumlahVar = hasil
+                    if (j != jumlahVar) {
+                        System.out.print("Masukkan koefisien variabel ke-" + j + ": ") ;
+                        matrix[i][j] = sc.nextInt() ;
+                    }
+                    else {
+                        System.out.print("Masukkan hasil persamaan : ") ;
+                        matrix[i][j] = sc.nextInt() ;
+                    }
+                }
+            }
+            return matrix ;
+
         }
         
     public static void main(String[] args){
-        readMatrix();
-        System.out.println(matrix[0][1]);
+        int[][] m = readMatrix();
+        System.out.println(m[0][1]);
     }
 }
